93a45615e4b629505e5a11ba7e629d3ab0f67153
Ocyn <niko.ocyn@gmail.com>
2024-11-20 17:43:57 +0100

    nginx fixed, env file working, wordpress now working, additionnal user added

--- srcs/wordpress/src/install.sh
+++ srcs/wordpress/src/install.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-wp core download;
+if ! wp-core is-installed; then
+	echo "Installing Wordpress\n";
+	wp core download;
 
-wp config create --dbname=wordpress_db --dbuser=wordpress_usr --dbpass=yuri --dbhost=mariadb --allow-root;
+	echo "Init Wordpress Database\n";
+	wp config create --dbname=${DB_NAME} \
+	--dbuser=${DB_USER} \
+	--dbpass=${DB_PASSWORD} \
+	--dbhost=${DB_HOST} \
+	--allow-root;
+	sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php82/php-fpm.d/www.conf
 
-sed -i "s|listen = 127.0.0.1:9000|listen = 0.0.0.0:9000|g" /etc/php82/php-fpm.d/www.conf
+	echo "Init Wordpress Admin\n";
+	wp core install --url=${WP_URL} \
+	--title=Inception \
+	--admin_user=${WP_ADMINNAME} \
+	--admin_password=${WP_ADMIN_PASSWD} \
+	--admin_email=${WP_ADMIN_MAIL} \
+	--skip-email;
 
-wp core install --url=jcuzin.42.fr --title=Inception --admin_user=admin --admin_password=yuri --admin_email=jcuzin@student.42lyon.fr --skip-email;
+	wp user create ${WP_USERNAME} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASSWD}
+fi
 
 
 php-fpm82 -F
-# lighttpd -D -f /etc/lighttpd/lighttpd.conf
